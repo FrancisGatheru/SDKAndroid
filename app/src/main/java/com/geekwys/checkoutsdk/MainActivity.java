ab8791737315779c6c9cbb15af6e201209aba571
ShemBrooklyne <shemnyatti@gmail.com>
2022-06-03 12:27:08 +0300

    initial commit

---
+++ app/src/main/java/com/geekwys/checkoutsdk/MainActivity.java
@@ -0,0 +1,17 @@
+package com.geekwys.checkoutsdk;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+* @author evil twins
+* */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
